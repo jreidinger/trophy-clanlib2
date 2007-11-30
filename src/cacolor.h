4347eeed0d00f796d4008eb9bb9cbfb3bbaae60d
lifo2 <lifo2@ef5f324a-0337-0410-b96f-b1d68aca83c0>
2007-11-30 20:10:22 +0000

    Prepare cacolor.h and class CAColor rename to utils/hsvcolor.h and HSVColor.
    git-svn-id: http://trophy.svn.sourceforge.net/svnroot/trophy@5 ef5f324a-0337-0410-b96f-b1d68aca83c0

--- src/cacolor.h
+++ src/cacolor.h
@@ -1,50 +1,50 @@
-#ifndef CACOLOR_H
-#define CACOLOR_H
+#ifndef HSVCOLOR_H
+#define HSVCOLOR_H
 
 #include <ClanLib/core.h>
 
-
 /** Structure for a color.
-    Colors in trophy are usually used for 
-    the modification of red based surfaces.
-    @author Andrew Mustun
-*/
-struct CAColor {
-
+  Colors in trophy are usually used for 
+  the modification of red based surfaces.
+  @author Andrew Mustun
+  */
+struct HSVColor 
+{
     /** Constructor for a black color.
     */
-    CAColor() {
+    HSVColor() 
+    {
         h = s = v = 0;
     }
 
-
     /** Constructor for any desired color.
     */
-    CAColor( int h, int s, int v ) {
+    HSVColor( int h, int s, int v ) 
+    {
         this->h = h;
         this->s = s;
         this->v = v;
     }
 
-
     /** Operator ==
     */
-    inline bool operator==( CAColor c ) {
+    inline bool operator==( HSVColor c ) 
+    {
         return (this->h==c.h && this->s==c.s && this->v==c.v);
     }
 
-
     /** Operator !=
     */
-    inline bool operator!=( CAColor c ) {
+    inline bool operator!=( HSVColor c ) 
+    {
         //return (this->h!=c.h || this->s!=c.s || this->v!=c.v);
         return !operator==( c );
     }
 
-
     /** Operator =
     */
-    inline CAColor operator=( CAColor c ) {
+    inline HSVColor operator=( HSVColor c ) 
+    {
         h = c.h;
         s = c.s;
         v = c.v;
