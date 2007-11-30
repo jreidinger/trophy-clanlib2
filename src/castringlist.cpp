dd007f51004a9f3c82173be94e0173d12acba25f
lifo2 <lifo2@ef5f324a-0337-0410-b96f-b1d68aca83c0>
2007-11-30 20:11:21 +0000

    Prepare CAStringList rename to StringList.
    git-svn-id: http://trophy.svn.sourceforge.net/svnroot/trophy@6 ef5f324a-0337-0410-b96f-b1d68aca83c0

--- src/castringlist.cpp
+++ src/castringlist.cpp
@@ -1,54 +1,47 @@
-#include "castringlist.h"
-
-
+#include "stringlist.h"
 
 /** Constructor.
 */
-CAStringList::CAStringList( char separator ) {
+StringList::StringList( char separator ) 
+{
     this->list = "";
     this->separator = separator;
 }
 
-
-
 /** Constructor.
 */
-CAStringList::CAStringList( const char* list, char separator ) {
+StringList::StringList( const char* list, char separator ) 
+{
     this->list = list;
     this->separator = separator;
 }
 
-
-
 /** Destructor.
 */
-CAStringList::~CAStringList() {}
-
-
+StringList::~StringList() {}
 
 /** Count.
 */
 int
-CAStringList::count() {
+StringList::count() 
+{
     int ret=1;
 
     for( unsigned int i = 0; i < list.length(); ++i ) 
     {
-        if( list[i]==separator ) {
+        if( list[i]==separator ) 
             ++ret;
-        }
     }
 
     return ret;
 }
 
-
-
 /** Returns item number 'which' of the list.
     \param maxLen Max length of the sting to return. Default: 128
 */
 const char*
-CAStringList::getItem( int which, int maxLen ) {
+StringList::getItem( int which, int maxLen ) 
+{
     unsigned int i = 0;
     int j = 0;
     int found = 0;
@@ -56,24 +49,19 @@ CAStringList::getItem( int which, int maxLen ) {
 
     while( i < list.length() && found < which ) 
     {
-        if( list[i]==separator ) {
+        if( list[i]==separator ) 
             ++found;
-        }
         ++i;
     }
 
     j = list.find( '~', i );
 
     if( j == -1 ) 
-    {
         j = list.length();
-    }
 
     item = list.substr( i, j-i );
 
     return item.c_str();
 }
-
-
 
 // EOF
--- src/castringlist.h
+++ src/castringlist.h
@@ -1,37 +1,35 @@
-#ifndef CASTRINGLIST_H
-#define CASTRINGLIST_H
+#ifndef STRINGLIST_H
+#define STRINGLIST_H
 
 #include <ClanLib/core.h>
 
-#define CA_SL_ITEMSIZE  128
+#define SL_ITEMSIZE  128
 
 /** A simple string list implemented by strings with given separators.
     It's not fast, not good, but simple.
     @author Andrew Mustun
 */
-class CAStringList {
-public:
-    CAStringList( char separator='~' );
-    CAStringList( const char* list, char separator='~' );
-    ~CAStringList();
+class StringList 
+{
+    public:
+        StringList( char separator='~' );
+        StringList( const char* list, char separator='~' );
+        ~StringList();
 
-    /** Sets the string list.
-        \param list The new list in format "entry 1~entry2~entry3".
-    */
-    void   setList( const char* list ) {
-        this->list = list;
-    }
-    int    count();
-    const char*  getItem( int which, int maxLen=CA_SL_ITEMSIZE );
+        /** Sets the string list.
+          \param list The new list in format "entry 1~entry2~entry3".
+          */
+        void   setList( const char* list ) { this->list = list; }
+        int    count();
+        const char*  getItem( int which, int maxLen=SL_ITEMSIZE );
 
-private:
-    //! The list as one string.
-
-    std::string list;
-    //! One item. Which depends on the last action.
-    std::string item;
-    //! Separator character (default: '~').
-    char   separator;
+    private:
+        //! The list as one string.
+        std::string list;
+        //! One item. Which depends on the last action.
+        std::string item;
+        //! Separator character (default: '~').
+        char   separator;
 };
 
 #endif
